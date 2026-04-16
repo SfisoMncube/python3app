15664faea99618afb4324f7b31281d4cd402fa04
Sfiso Mncube <wandile@localhost.localdomain>
2026-04-16 21:09:56 +0200

    Adds python app with k8s

---
+++ src/app.py
@@ -0,0 +1,24 @@
+#'/api/v1/details'
+#'/api/v1/health'
+from flask import Flask
+import datetime,socket
+
+app = Flask(__name__)
+
+@app.route("/api/v1/details")
+def details():
+    return {
+        "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "hostname": socket.gethostname()
+
+    }
+
+@app.route("/api/v1/health")
+def health():
+    return {
+        "status": "up"
+
+    },200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
