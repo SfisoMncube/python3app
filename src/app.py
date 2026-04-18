0d8a03fe6227abcc3fda15a9991204ee750114eb
Sfiso Mncube <wandile@localhost.localdomain>
2026-04-18 04:44:58 +0200

    automated cd

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def details():
         "hostname": socket.gethostname(),
         "message": "You are doing Great!!",
         "deployed_on": "K8s",
-        "deployed_for": "Cloud Platform Engineer"
+        "deployed_for": "Platform Engineer"
 
     }
 
